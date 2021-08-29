1126915bece1fae93b58fa6078048785eb94eb0c
DanyOlous <danidan520@hotmail.fr>
2021-08-30 00:15:50 +0200

    BST - add contains and remove methods + tests

--- practice-DSA/src/app/BinaryTree/BinarySearchTree.java
+++ practice-DSA/src/app/BinaryTree/BinarySearchTree.java
@@ -8,35 +8,92 @@ public final class BinarySearchTree<E extends Comparable<E>> extends BinaryTree<
 
     public BinarySearchTree() {
         super();
+        root = super.root;
     }
 
     public BinarySearchTree(E elt) {
         super(elt);
+        root = super.root;
     }
 
     @Override
     public void insert(E elt) {
         insertMethod(root, elt);
     }
-
-    private void insertMethod(TreeNode<E> node, E elt) {
-        if (empty())
-            node = new TreeNode<E>(elt);
+    private TreeNode<E> insertMethod(TreeNode<E> node, E elt) {
+        if (node == null || empty())
+            return new TreeNode<E>(elt);
         else if (elt.compareTo(node.data) < 0)
-            insertMethod(node.left, elt);
+            node.left = insertMethod(node.left, elt);
         else
-            insertMethod(node.right, elt);
+            node.right = insertMethod(node.right, elt);
+        return node;
+    }
+
+    @Override
+    public void remove(E elt) {
+        delete(root, elt);
+    }
+    private TreeNode<E> delete(TreeNode<E> node, E elt) {
+        if (node == null || empty())
+            return node;
+        if (elt.compareTo(node.data) < 0)
+            node.left = delete(node.left, elt);
+        else if (elt.compareTo(node.data) > 0)
+            node.right = delete(node.right, elt);
+        else {
+            if (node.left == null)
+                return node.right;
+            else if (node.right == null)
+                return node.left;
+            else {
+                node = minValue();
+                node.right = delete(node.right, node.data);
+                node = null;
+        
+            }
+        }
+        return node;
+    }
+    /*Node deleteRec(Node root, int key)
+    {
+        if (root == null)
+            return root;
+        if (key < root.key)
+            root.left = deleteRec(root.left, key);
+        else if (key > root.key)
+            root.right = deleteRec(root.right, key);
+        else {
+            if (root.left == null)
+                return root.right;
+            else if (root.right == null)
+                return root.left;
+            root.key = minValue(root.right);
+            root.right = deleteRec(root.right, root.key);
+        }
+        return root;
+    }*/
+ 
+    private TreeNode<E> minValue()
+    {
+        TreeNode<E> tmp = root;
+        TreeNode<E> minValue = tmp;
+        while (tmp.left != null)
+        {
+            minValue = tmp.left;
+            tmp = tmp.left;
+        }
+        return minValue;
     }
 
     @Override
     public boolean contains(E elt) {
         return containsMethod(root, elt);
     }
-
     private boolean containsMethod(TreeNode<E> node, E elt) {
-        if (empty())
+        if (node == null || empty())
             return false;
-        else if (elt.compareTo(node.data) < 0)
+        else if (elt.compareTo(node.data) == 0)
             return true;
         else if (elt.compareTo(node.data) < 0)
             return containsMethod(node.left, elt);
--- practice-DSA/src/test/BinaryTree/BinarySearchTreeTest.java
+++ practice-DSA/src/test/BinaryTree/BinarySearchTreeTest.java
@@ -1,5 +1,62 @@
 package test.BinaryTree;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.*;
+import app.BinaryTree.TreeNode;
+import app.BinaryTree.BinarySearchTree;
+import app.BinaryTree.BinaryTree;
+
+/**
+ * Created by DanyOlous on 29.08.2021.
+ */
 public class BinarySearchTreeTest {
+
+    @Test
+    public void BstTest1() {
+        BinarySearchTree<Integer> BST = new BinarySearchTree<Integer>();
+        assertEquals(true, BST.empty());
+
+        BST.getNode().setData(13);
+        assertEquals(13, BST.getNode().getData());
+        assertEquals(true, BST.contains(13));
+        assertEquals(1, BST.size());
+        assertEquals(0, BST.height());
+
+        BST.insert(1);
+        assertEquals(1, BST.height());
+        assertEquals(2, BST.size());
+
+        BST.insert(16);
+        BST.insert(77);
+        assertEquals(true, BST.contains(13));
+        assertEquals(true, BST.contains(1));
+        assertEquals(true, BST.contains(16));
+        assertEquals(true, BST.contains(77));
+        assertEquals(false, BST.contains(123));
+        assertEquals("1 77 16 13 ", BST.postOrder());
+        assertEquals("13 1 16 77 ", BST.preOrder());
+        assertEquals("13 1 16 77 ", BST.levelOrder());
+        assertEquals("1 13 16 77 ", BST.inOrder());
+        assertEquals(4, BST.size());
+        assertEquals(2, BST.height());
+
+        assertEquals(4, BST.size());
+        BST.remove(76);
+        assertEquals("13 1 16 77 ", BST.levelOrder());
+        assertEquals(4, BST.size());
+        BST.remove(77);
+        assertEquals("13 1 16 ", BST.levelOrder());
+        assertEquals(3, BST.size());
+        assertEquals(false, BST.contains(77));
+        assertEquals("13 1 16 ", BST.levelOrder());
+
+        BST.insert(100);
+        assertEquals(4, BST.size());
+        assertEquals(true, BST.contains(100));
+        BST.remove(1);
+        assertEquals(3, BST.size());
+        assertEquals(false, BST.contains(1));
+        assertEquals("13 16 100 ", BST.levelOrder());
+    }
     
 }
