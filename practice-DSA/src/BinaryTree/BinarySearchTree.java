8a36efe0d1fee393a4e227947e7c2b32faa30d6b
DanyOlous <danidan520@hotmail.fr>
2021-08-29 03:36:20 +0200

    BST - add insert, contains and constructors methods

--- practice-DSA/src/BinaryTree/BinarySearchTree.java
+++ practice-DSA/src/BinaryTree/BinarySearchTree.java
@@ -1,5 +1,46 @@
 package BinaryTree;
 
-public class BinarySearchTree<T> extends BinaryTree<T> {
-    
+/**
+ * Created by DanyOlous on 29.08.2021.
+ */
+public final class BinarySearchTree<E extends Comparable<E>> extends BinaryTree<E> {
+    TreeNode<E> root;
+
+    public BinarySearchTree() {
+        super();
+    }
+
+    public BinarySearchTree(E elt) {
+        super(elt);
+    }
+
+    @Override
+    public void insert(E elt) {
+        insertMethod(root, elt);
+    }
+
+    private void insertMethod(TreeNode<E> node, E elt) {
+        if (empty())
+            node = new TreeNode<E>(elt);
+        else if (elt.compareTo(node.data) < 0)
+            insertMethod(node.left, elt);
+        else
+            insertMethod(node.right, elt);
+    }
+
+    @Override
+    public boolean contains(E elt) {
+        return containsMethod(root, elt);
+    }
+
+    private boolean containsMethod(TreeNode<E> node, E elt) {
+        if (empty())
+            return false;
+        else if (elt.compareTo(node.data) < 0)
+            return true;
+        else if (elt.compareTo(node.data) < 0)
+            return containsMethod(node.left, elt);
+        else
+            return containsMethod(node.right, elt);
+    }
 }
