6e9551745eecf2e34ae21cb56cd0f0cf58aeaa25
DanyOlous <danidan520@hotmail.fr>
2021-08-31 17:06:00 +0200

    [Trie] - create Trie Class + constructor and attribute

---
+++ practice-DSA/src/app/Trie/Trie.java
@@ -0,0 +1,21 @@
+package app.Trie;
+
+public class Trie implements ITrie{
+
+    TrieNode root;
+
+    public Trie() {
+        root = new TrieNode(' ');
+    }
+    public void insert(String string) {
+
+    }
+
+    public void remove(String string) {
+
+    }
+
+    public boolean find(String string) {
+        return false;
+    }
+}
