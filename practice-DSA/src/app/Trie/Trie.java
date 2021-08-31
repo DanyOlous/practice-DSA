7e2b61786955ea0709121515bde59451552ffd4e
DanyOlous <danidan520@hotmail.fr>
2021-09-01 00:28:11 +0200

    [Trie] -  add method suffix & find

--- practice-DSA/src/app/Trie/Trie.java
+++ practice-DSA/src/app/Trie/Trie.java
@@ -1,5 +1,8 @@
 package app.Trie;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class Trie implements ITrie{
 
     TrieNode root;
@@ -9,6 +12,8 @@ public class Trie implements ITrie{
     }
     public void insert(String str) {
         TrieNode tmp = root;
+        if (findWord(str))
+            return;
         for(int i = 0; i < str.length(); i++) {
             if (tmp.getChildren().get(str.charAt(i)) != null)
                 tmp.addFrequency();
@@ -20,18 +25,61 @@ public class Trie implements ITrie{
     }
 
     public void remove(String str) {
+        if (!findWord(str))
+            return;
         TrieNode tmp = root;
         for (int i = 0; i < str.length(); i++) {
-            if (tmp.getChildren().get(str.charAt(i)) != null)
-                tmp.reduceFrequency();
-            else
-                tmp.getChildren().put(str.charAt(i), new TrieNode(str.charAt(i)));
+            tmp.reduceFrequency();
             tmp = tmp.getChildren().get(str.charAt(i));
         }
         tmp.unsetIsWord();
     }
 
-    public boolean find(String string) {
-        return false;
+    public boolean findWord(String str) {
+        TrieNode tmp = root;
+        for (int i = 0; i < str.length(); i++) {
+            if (tmp.getChildren().get(str.charAt(i)) != null)
+                tmp = tmp.getChildren().get(str.charAt(i));
+            else
+                return false;
+        }
+        return tmp.getIsWord();
+    }
+
+    public boolean find(String str) {
+        TrieNode tmp = root;
+        for (int i = 0; i < str.length(); i++) {
+            if (tmp.getChildren().get(str.charAt(i)) != null) {
+                tmp.reduceFrequency();
+                tmp = tmp.getChildren().get(str.charAt(i));
+            }
+            else
+                return false;
+        }
+        return true;
+    }
+
+    public int countSuffix(String str) { 
+        if (!find(str))
+            return 0;
+        TrieNode tmp = root;
+        for (int i = 0; i < str.length(); i++) {
+            tmp.reduceFrequency();
+            tmp = tmp.getChildren().get(str.charAt(i));
+        }
+        return tmp.getFrequency();
+    }
+
+    public ArrayList<String> suffix(String str) {
+        ArrayList<String> wordList = new ArrayList<String>();
+        if (!find(str))
+            return wordList;
+        TrieNode tmp = root;
+        for (int i = 0; i < str.length(); i++) {
+            tmp.reduceFrequency();
+            tmp = tmp.getChildren().get(str.charAt(i));
+        }
+        stack<TrieNode> s = new Stack<TrieNode>();
+        return wordList;
     }
 }
